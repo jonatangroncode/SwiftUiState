99761a33abbac3cedd93ec97ab9f6961b0fef6de
jonatan <jonatan.gron.code@hotmail.com>
2023-01-13 14:36:57 +0100

    Initial Commit

---
+++ SwiftUiState/SwiftUiStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStateApp.swift
+//  SwiftUiState
+//
+//  Created by Admin on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
