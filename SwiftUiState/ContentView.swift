9491566c442d1c70a6fb3a131f237bc40d1f20d0
jonatan <jonatan.gron.code@hotmail.com>
2023-01-13 21:23:34 +0100

    first commit

--- SwiftUiState/ContentView.swift
+++ SwiftUiState/ContentView.swift
@@ -7,10 +7,20 @@
 
 import SwiftUI
 
+//I struct får man inte ändra variabler för att dom skapas om hela tiden, så jag måste ha ett state
 struct ContentView: View {
+    @State var count = 0 //nu skapas alla views om som är beroende av variablen när den ändras
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("\(count)") // beroende av State counter 
+            Button(action: {
+                count += 1
+            }) {
+                Text("Click")
+            }
+        }
+        
     }
 }
 
